e69f6ac31a182ffc15ad51f9d065b8fcb7329feb
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2018-01-08 22:29:42 -0500

    get all industry sectors

---
+++ api/controllers/industrySectors.js
@@ -0,0 +1,49 @@
+const Promise = require("bluebird");
+const db = require("../../models");
+
+const { IndustrySector, Applicant, ApplicantIndustrySector } = db;
+
+const getAllIndustrySectors = (req, res, next) => {
+  IndustrySectors.findAll()
+    .then(skills => {
+      res.json({
+        successful: true,
+        data: {
+          skills
+        },
+        status: 200
+      });
+    })
+    .catch(next);
+};
+
+const addIndustrySectorsToApplicant = (req, res, next) => {
+  const applicantId = req.swagger.params.applicantId.value;
+  const { skills } = req.body;
+  Applicant.findById(applicantId)
+    .then(applicant => {
+      return Promise.map(skills, skill => {
+        console.log(ApplicantIndustrySector);
+        return ApplicantIndustrySector.create({
+          ApplicantId: applicant.id,
+          IndustrySectorId: skill.id,
+          yearsExperience: skill.yearsExperience
+        });
+      });
+    })
+    .then(skills => {
+      res.json({
+        successful: true,
+        data: {
+          skills
+        },
+        status: 201
+      });
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getAllIndustrySectors,
+  addIndustrySectorsToApplicant
+};
