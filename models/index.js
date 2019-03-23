1f34836c4d4f69ab7faff0e1473834119efbaa86
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2019-03-23 13:34:00 -0400

    DB setup

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const config = require("../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
